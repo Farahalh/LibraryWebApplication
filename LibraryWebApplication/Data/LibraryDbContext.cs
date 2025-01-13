00a417cd19a991b85842eb843f9ab4c75c6d0eeb
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-01-13 16:59:10 +0100

    Updating DB context to reflect current DB structure and tables.

--- LibraryWebApplication/Data/LibraryDbContext.cs
+++ LibraryWebApplication/Data/LibraryDbContext.cs
@@ -7,24 +7,20 @@ namespace LibraryApi.Data
     public class LibraryDbContext : DbContext
     {
         public DbSet<Author> Authors { get; set; }
-        public DbSet<Book> Books { get; set; }
-        public DbSet<BookCopy> BooksCopy { get; set; }
+        public DbSet<Book> Books { get; set; } 
         public DbSet<Borrower> Borrowers { get; set; }
-        public DbSet<Category> Categories { get; set; }
         public DbSet<Checkout> Checkout { get; set; }
-        public DbSet<Hold> Holds { get; set; }
-        public DbSet<Notification> Notifications { get; set; }
-        public DbSet<Publisher> Publishers { get; set; }
 
-        public LibraryDbContext()
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
         {
 
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(@"Server=ITHS-HP-EB-059\SQLEXPRESS;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;")
-            .LogTo(message => Debug.WriteLine(message))
+            
+            options.LogTo(message => Debug.WriteLine(message))
                 .EnableSensitiveDataLogging();
         }
     }
